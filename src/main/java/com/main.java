93ba56a1974d3a5cb5de216dcfc7244a5f471495
ViMatviienko <viktoriia.mataviienko@wdc.com>
2022-06-08 19:34:35 +0200

    first test commit

---
+++ src/main/java/com/main.java
@@ -0,0 +1,13 @@
+package com;
+
+public class main {
+    public String wrong_naming_;
+
+    private int hardcoding = 7;
+
+    public static void Main(String[] args) {}
+
+    public void set_method_naming_(String firstName, String lastName, String streetAddress,
+            String city, String zipCode, String state, String country, String phoneNumber) {
+    }
+}
